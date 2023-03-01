ce725c4851068052dd516540b44f2e02e554d105
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-03-01 16:45:50 -0700

    bloc, event, state

---
+++ lib/bloc/user/user_state.dart
@@ -0,0 +1,13 @@
+part of 'user_bloc.dart';
+
+@immutable
+abstract class UserState {
+  final bool existUser;
+  final Usuario? user;
+
+  const UserState({this.existUser = false, this.user});
+}
+
+class UserInitialState extends UserState {
+  const UserInitialState() : super(existUser: false, user: null);
+}
